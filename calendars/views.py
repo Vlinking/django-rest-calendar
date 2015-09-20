d263bed90ed0c3a92dc12b40fbb5dfd86a5203a1
Bartek <vlinking@gmail.com>
2015-09-20 23:55:37 +0200

    Displaying month

--- calendars/views.py
+++ calendars/views.py
@@ -1,5 +1,43 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+from calendar import Calendar
+from datetime import datetime
+
+
+MONTHS = [
+    '',
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień'
+]
+
+
 def index(request):
-    context = {}
+    now = datetime.now()
+    cal = Calendar()
+    current_month = cal.monthdays2calendar(now.year, now.month)
+    context = {
+        'current_month': current_month,
+        'month': MONTHS[now.month],
+        'year': now.year
+    }
+
     return render(request, 'calendars/index.html', context)
+
+
+class CustomCalendar(Calendar):
+    pass
+
+
+
+
