44294ed6b36126a60cf5c9602fdc171795480a30
Bartek <vlinking@gmail.com>
2015-09-27 19:59:08 +0200

    allowing to edit if shared event and has edit rights

--- calendars/permissions.py
+++ calendars/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from calendars.models import CalendarSharing
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -8,5 +9,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        try:
+            CalendarSharing.objects.get(calendar=obj.calendar, recipient=request.user, type='W')
+        except CalendarSharing.DoesNotExist:
+            shared = False
+        else:
+            shared = True
 
-        return obj.calendar.owner == request.user
+        return obj.calendar.owner == request.user or shared
